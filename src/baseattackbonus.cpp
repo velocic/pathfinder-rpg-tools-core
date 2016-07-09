2088ced29af6d7b9c3b0541c126e164be5bca9b7
velocic <velocicdev@gmail.com>
2016-07-08 21:35:29 -0400

    Remove some accidental whitespace

--- src/baseattackbonus.cpp
+++ src/baseattackbonus.cpp
@@ -7,7 +7,7 @@ namespace RulesEngine
         void BaseAttackBonus::calculateBaseAttackBonus()
         {
         }
-        
+
         void BaseAttackBonus::notifyObservers(const std::string& fieldName)
         {
             for (auto observer : observers) {
@@ -20,22 +20,21 @@ namespace RulesEngine
             //Stubbed for now
             return 0;
         }
-        
+
         void BaseAttackBonus::receiveNotification(const ObserverSubject* subject, const std::string& fieldName)
         {
             //Needs to subscribe to updates about class levels (from any number of classes, with
             //  any combination of BAB progression types) from the characterdescription module
         }
-        
+
         void BaseAttackBonus::registerObserver(const std::string& observerName, Observer* observer)
         {
             observers.insert(std::make_pair(observerName, observer));
         }
-        
+
         void BaseAttackBonus::unregisterObserver(const std::string& observerName)
         {
             observers.erase(observerName);
         }
-        
     }
 }
