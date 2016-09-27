a346a2d9a8e585c037d4745b1192636c14086c44
velocic <velocicdev@gmail.com>
2016-09-26 21:31:01 -0400

    Stubbing out tests for character description section. Only really applies to NegativeLevel functions, since most of this file is pretty basic

---
+++ tests/characterdescriptiontests.cpp
@@ -0,0 +1,66 @@
+#include <testcommon.h>
+#include <characterdescription.h>
+
+using namespace RulesEngine::Character;
+
+TEST(CharacterDescription, AddTemporaryNegativeLevelDebuffUpdatesTotalNegativeLevelCount)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, AddPermanentNegativeLevelDebuffUpdatesTotalNegativeLevelCount)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, AddTemporaryNegativeLevelsUpdatesTotalNegativeLevelCount)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, AddPermanentNegativeLevelsUpdatesTotalNegativeLevelCount)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, RemoveTemporaryNegativeLevelsUpdatesTotalNegativeLevelCount)
+{
+    //Test cases: count > 0, count reduced to exactly 0, try to reduce to a negative number
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, RemovePermanentNegativeLevelsUpdatesTotalNegativeLevelCount)
+{
+    //Test cases: count > 0, count reduced to exactly 0, try to reduce to a negative number
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, ToggleTemporaryNegativeLevelDebuffUpdatesTotalNegativeLevelCount)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, TogglePermanentNegativeLevelDebuffUpdatesTotalNegativeLevelCount)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, GetTemporaryNegativeLevelDebuffReturnsProperDebuffEntry)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, GetPermanentNegativeLevelDebuffReturnsProperDebuffEntry)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, GetTemporaryNegativeLevelDebuffsReturnsEntireCollectionProperly)
+{
+    throw std::logic_error("Unimplemented");
+}
+
+TEST(CharacterDescription, GetPermanentNegativeLevelDebuffsReturnsEntireCollectionProperly)
+{
+    throw std::logic_error("Unimplemented");
+}
